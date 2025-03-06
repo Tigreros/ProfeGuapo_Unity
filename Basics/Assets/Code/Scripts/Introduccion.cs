87ca44eb9f2c7108930598d4f7d38c7acb694dd9
Tigreros <130391060+Tigreros@users.noreply.github.com>
2025-03-06 19:02:35 +0100

    Clase definición de variables
    • Creación estructura de carpetas
    • Crear script introducción
    • Hemos modificado la posición de la esfera que había en escena

---
+++ Basics/Assets/Code/Scripts/Introduccion.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Introduccion : MonoBehaviour
+{
+
+    int edadProfeGuapo = 29; // CamelCase
+    float velocidadEnvejecimientoProfeGuapo = 5.2f; // Recordar colocar la "f"
+    bool elProfeEsGuapo = true; // Esta es la variable más optima de la muerte
+    string cadenaDeCaracteres = "Profe es muy guapo"; // concatenación o lista o array de carácteres
+    char carater = 'A'; 
+
+
+    // int EdadProfeGuapo = 29; // PascalCase
+
+
+    // se ejecuta antes del start
+    void Awake()
+    {
+        
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Esta función solo se llama una vez, en el primer frame del juego
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
