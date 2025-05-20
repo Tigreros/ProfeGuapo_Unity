dfd485efd6f41c28ca52d0533b6df676a068b7fa
Tigreros <130391060+Tigreros@users.noreply.github.com>
2025-05-20 19:22:58 +0200

    wearara

--- C/Calculadora/main.c
+++ C/Calculadora/main.c
@@ -3,29 +3,41 @@
 #include <math.h>
 
 int main() {
-	int a, b;
-	char operaciones;
+
+	int x = 5;		// variable normal
+	int* p = &x;	// puntero que guarda la dirección de x
+
+	printf("Valor de X: %d", x);
+
+	printf("\nDirección de X: %p: ", (void*)&x);
+
+	printf("\nValor al que apunta p (*p): %d: ", *p);
+
+	scanf_s("%d", &x);
+
+	//x = 30;
+	// Modificando el valor de x a través del puntero
+
+	printf("\nDespues de X: %d", x);
+
+	printf("\nDespues direccion de X: %p: ", (void*)&x);
+
+	printf("\nDespues al que apunta p (*p): %d: \n\n\n", *p);
+
+	scanf_s("%d", p);
+
+
+	printf("\nDespues de X: %d", x);
+
+	printf("\nDespues direccion de X: %p: ", (void*)&x);
+
+	printf("\nDespues al que apunta p (*p): %d: \n\n\n", *p);
+
+	return 0;
+}
 
 
 
-	// Calculo de volumen de un cubo
-	printf("Introduce el valor del lado de tu cubo : ");
-
-	scanf_s("%d", &a);
-
-	float area = mult(a, a);
-	float volumen = mult(area, a);
-
-	printf("El volumen del cubo es: %f", volumen);
-
-
-	// Calculo de lo que tardaria el bala en recorrer la hipotenusa de ese cubo
-
-
-	float distancia = a * sqrt(3);
-
-	//printf("\nla bala recorre : %f tanos metros", distancia);
-	printf("\n\n\nla bala recorre : %f metros", distancia);
 
 
 
@@ -41,6 +53,85 @@ int main() {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//int a, b;
+	//char operaciones;
+
+
+
+	//// Calculo de volumen de un cubo
+	//printf("Introduce el valor del lado de tu cubo : ");
+
+	//scanf_s("%d", &a);
+
+	//float area = mult(a, a);
+	//float volumen = mult(area, a);
+
+	//printf("El volumen del cubo es: %f", volumen);
+
+
+	//// Calculo de lo que tardaria el bala en recorrer la hipotenusa de ese cubo
+
+
+	//float distancia = a * sqrt(3);
+
+	////printf("\nla bala recorre : %f tanos metros", distancia);
+	//printf("\n\n\nla bala recorre : %f metros", distancia);
+
+
+
+
+
+
+
+
+	//int x = 10;
+
+	//int* p = &x;
+
+
+
+
+	//int* p = &x;
 
 
 
@@ -78,5 +169,5 @@ int main() {
 	//	}
 
 	//printf("el resultado de tu operacion es: %f \n ", resultado);
-	return 0;
-}
+
+//}
